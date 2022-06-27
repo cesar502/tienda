79090af8ac37c2f47ae4a8e132edb4959c9aa490
cesar502 <106337766+cesar502@users.noreply.github.com>
2022-06-26 21:05:40 -0500

    Add files via upload

---
+++ dialogos.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class dialogos : MonoBehaviour{
+  
+  public TextMeshProUGUI textDisplay;
+  public string[] sentences;
+  private int index;
+  public float typingSpeed;
+
+  void start(){
+    StartCoroutine (Type());
+    }
+
+
+  IEnumerator Type(){
+ 
+     foreach(char letter in sentences[index].ToCharArray()){
+     textDisplay.text += letter;
+     yield return new WaitForSeconds(typingSpeed);
+    }
+ 
+  }
+    public void NextSentence(){
+
+        if(index < sentences.Length -1){
+       index++;
+       textDisplay.text = "";
+       StartCoroutine(Type());
+    } 
+    else 
+    {
+        textDisplay = "";
+
+            }
+ 
+  } 
+
+
+}
